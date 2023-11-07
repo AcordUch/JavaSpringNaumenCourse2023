0f8b00caf5e3f5090bb49584d423ded514f3d23e
da.evdokimov <da.evdokimov@tinkoff.ru>
2023-11-07 19:23:40 +0500

    use room repository

--- src/main/java/com/acord/dealweb/services/RoomServiceLive.java
+++ src/main/java/com/acord/dealweb/services/RoomServiceLive.java
@@ -1,43 +1,43 @@
 package com.acord.dealweb.services;
 
 import com.acord.dealweb.domain.Room;
-import lombok.val;
-import org.springframework.stereotype.Service;
-
+import com.acord.dealweb.repositories.RoomRepository;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class RoomServiceLive implements RoomService {
-    @Override
-    public void add(Room room) {
+  private final RoomRepository roomRepository;
 
-    }
+  @Autowired
+  public RoomServiceLive(RoomRepository roomRepository) {
+    this.roomRepository = roomRepository;
+  }
 
-    @Override
-    public Room getOne(String id) {
-        val map = new HashMap<String, Room>();
-        map.put("1", Room.builder().name("11").uuid("1").build());
-        map.put("2", Room.builder().name("kek").uuid("2").build());
-        return map.get(id);
-    }
+  @Override
+  public void addOrUpdate(Room room) {
+    roomRepository.save(room);
+  }
 
-    @Override
-    public List<Room> getAll() {
-        return List.of(
-                Room.builder().name("11").uuid("1").build(),
-                Room.builder().name("kek").uuid("2").build()
-        );
-    }
+  @Override
+  public Room getOne(String id) {
+    val map = new HashMap<String, Room>();
+    map.put("1", Room.builder().name("11").uuid("1").build());
+    map.put("2", Room.builder().name("kek").uuid("2").build());
+    return map.get(id);
+  }
 
-    @Override
-    public void update(String id, Room room) {
+  @Override
+  public List<Room> getAll() {
+    return List.of(
+        Room.builder().name("11").uuid("1").build(), Room.builder().name("kek").uuid("2").build());
+  }
 
-    }
-
-    @Override
-    public void delete(String id) {
-
-    }
+  @Override
+  public void delete(String id) {
+    roomRepository.deleteById(id);
+  }
 }
